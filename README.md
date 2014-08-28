250ec94b31e8bd72c3bcc0e41b63f957b2b96a48
Gerwin Brunner <gerwin.brunner@gmail.com>
2014-08-28 11:28:53 +0200

    updated handsontable to 0.11.0.beta3

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-handsontable-latest
+handsontable
 ===================
 
 [Handsontable](http://handsontable.com/) is a minimalistic Excel-like data grid editor for HTML, JavaScript & jQuery.
 
 It supports [these](https://github.com/warpech/jquery-handsontable/wiki/Keyboard-Shortcuts) keyboard shortcuts.
 
-###Installation
-With [Meteorite](https://github.com/oortcloud/meteorite) installed:
-
-```sh
-$ mrt add handsontable-latest
-```
 
 For documentation and demo: [See here](http://handsontable.com/demo/understanding_reference.html).
 
--- package.js
+++ package.js
@@ -1,15 +1,19 @@
 /* 
 * @Author: sg
 * @Date:   2014-06-12 13:51:42
-* @Last Modified by:   sg
-* @Last Modified time: 2014-06-26 18:06:07
-* @Email: sujith3g@gmail.com 
+* @Last Modified by:   gb
+* @Last Modified time: 2014-08-28 10:36:00
+* @Email: gerwin.brunner@vilango.com 
 */
 Package.describe({
-summary: "Handsontable 0.10.5 for meteor"
+  summary: "Handsontable 0.11.0-beta3 for meteor",
+  version: "0.11.0-beta3-a",
+  name: "gbrunner:handsontable",
+  git: "https://github.com/Vilango/meteor-handsontable.git"
 });
-Package.on_use(function (api, where) {
+Package.onUse(function (api, where) {
+  api.versionsFrom('METEOR@0.9.0');
 	api.use(['jquery'], 'client');
-	api.add_files(['libs/jquery.handsontable.full.js','libs/jquery.handsontable.full.css'], 'client');
+	api.addFiles(['libs/jquery.handsontable.full.js','libs/jquery.handsontable.full.css'], 'client');
 	api.export("Handsontable");
 });
